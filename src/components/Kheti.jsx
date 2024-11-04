9f46776638794e3ebea959d61e07fcbb6f3a79ae
sairampatra <saip01798@gmail.com>
2024-11-04 18:12:44 +0530

    kheti

--- src/components/Kheti.jsx
+++ src/components/Kheti.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import videoFile from '../assets/Kheti.mp4';
+// import videoFile from '../assets/Kheti.mp4';
 function Kheti() {
    
   return (
     <div className='relative left-[150%] flex flex-col justify-center items-center'>
        <h1 className='text-yellow-400 font-mono text-5xl '>kheti</h1>
-      <video 
+      {/* <video 
       className='video-custom'
        controls     // Adds playback controls (play, pause, volume, etc.)
        autoPlay     // Starts playing automatically when loaded
        loop         // Repeats the video when it ends
        muted        // Mutes the video (necessary for autoplay in most browsers)
        style={{ width: '100%', height: 'auto' }} //
-      src={videoFile}></video>
+      src={videoFile}></video> */}
      
 
     </div>
