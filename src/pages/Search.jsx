e4b0be01f257d7fc9edcdd6218c6be2c2d1e258d
sairampatra <saip01798@gmail.com>
2024-11-05 23:47:02 +0530

    kheti

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -33,7 +33,7 @@ function Search() {
     <div className={`grid w-full  grid-cols-4 gap-3 p-3 relative mt-[64px] ${darkMode? 'bgDark':'bgLight' }`}>
       {isLoading
         ? Array.from({ length: 8 }).map((item, i) => <SkeletonMovieCard key={i}/>)
-        : bhalu === 'sanksruti' || bhalu === 'Sanksruti' || bhalu === 'sanksruti sahoo' ||bhalu === 'Sanksruti sahoo' || bhalu === 'Sanksruti Sahoo'? <Kheti/> :
+        : bhalu === 'sanskruti' || bhalu === 'Sanskruti'|| bhalu === 'sanskruti sahoo' || bhalu === 'Sanskruti Sahoo'? <Kheti/> :
        
         data.map((movie, id) => {
             return <MovieCard key={id} movie={movie} />;
